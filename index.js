32d481a8d5104e87de63842828655c378ec2d189
veera-1729 <veerabhadrudu1729@gmail.com>
2022-08-11 19:38:32 +0530

    adding routes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
@@ -8,9 +9,10 @@ const todos = require('./models/task')
 const ejsMate = require('ejs-mate')
 const layout = require('express-ejs-layouts')
 const flash = require('connect-flash')
+const auth = require('./routes/autentication/index')
 
 //connecting to mongo
-mongoose.connect("mongodb+srv://VeeraBhadrudu:Veera%409989@cluster0.akdpvfn.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect(process.env.mongodburl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     if (!err)
         console.log("Connected to DB")
     else
@@ -30,7 +32,11 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 app.use(express.urlencoded({ extended: true }))//this statement parses the body of the incoming request
-app.use(session({ secret: "notagoodsecret" }))
+app.use(session({
+    secret: "notagoodsecret",
+    resave: false,
+    saveUninitialized: true,
+}))
 app.use(flash())
 app.use((req, res, next) => {
     res.locals.success = req.flash('success')
@@ -38,19 +44,22 @@ app.use((req, res, next) => {
     next()
 })
 
+app.use('/auth', auth)
+ 
+
 
 //adding a new todo
-app.post('/addtodo/:id',requireLogin, async (req, res) => {
+app.post('/addtodo/:id', requireLogin, async (req, res) => {
     //console.log("posting")
     const foundUser = await User.findById(req.params.id)
     const task = await todos.create(req.body.todo);
     task.user = foundUser.username
-    await task.save() 
+    await task.save()
     foundUser.tasks.push(task)
     await foundUser.populate('tasks')
     await foundUser.save()
     const data = foundUser.tasks
-    
+
     //req.flash('success','Successfully added new task')
 
 
@@ -60,105 +69,46 @@ app.post('/addtodo/:id',requireLogin, async (req, res) => {
 app.get('/', (req, res) => {
     //res.send('THIS IS THE HOME PAGE')
     res.render('index')
-})
-
-app.get('/register', (req, res) => {
-    res.render('register')
-})
-
-//saving the new user into database
-app.post('/register', async (req, res) => {
-
-    const { password, username } = req.body
-    const isvalid = await User.findOne({username:username})
-    if(isvalid != null)
-    {
-        req.flash('error','username already exists')
-        res.redirect('/register')
-    }
-    const newuser = new User({ username, password })
-    //pre method in user model is called before newuser is going to be saved 
-    await newuser.save();
-    const foundUser = newuser
-    req.session.user_id = newuser._id
-
-    await foundUser.populate('tasks')
-    await foundUser.save()
-    const data = foundUser.tasks
-    //req.flash('success',"Registration Successfull!!")
-    res.render('home', { foundUser, data })
-})
-
-//displaying the login page
-app.get('/login', (req, res) => { 
-    res.render('login')
-})
-
-//validating the user to login
-app.post('/login', async (req, res) => {
-
-    const { username, password } = req.body
-    const foundUser = await User.findOne({ username })
-    if(foundUser != null){
-    const isValid = await bcrypt.compare(password, foundUser.password)
-    if (isValid) {
-        req.session.user_id = foundUser._id
-
-        await foundUser.populate('tasks')
-        await foundUser.save()
-        const data = foundUser.tasks
-
-        res.render('home', { foundUser, data })
-    }
-    else {
-        req.flash('error','Invalid Username or password')
-        res.redirect('register')
-    }}
-    else {
-        req.flash('error','Invalid Username or password')
-        res.redirect('register')
-    }
-})
-
-
-
-//loggin out some one
-app.get('/logout',requireLogin, (req, res) => {
-    req.session.user_id = null 
-    req.flash('success',"Logged out Successfully")
-    res.redirect('/login')
-})
+}) 
 
 //editing the tasks
-app.get('/edit/:id/:tid', requireLogin,async (req, res) => {
+app.get('/edit/:id/:tid', requireLogin, async (req, res) => { 
 
-
-    const foundUser = await User.findById(req.params.id) 
-    const task = await todos.findOne({ user: foundUser.username, _id: req.params.tid })  
+    const foundUser = await User.findById(req.params.id)
+    const task = await todos.findOne({ user: foundUser.username, _id: req.params.tid })
     res.render('edit', { task, foundUser })
 })
 
 //saving the edited details 
-app.post('/edit/:id/:tid',requireLogin, async (req, res) => {
+app.post('/edit/:id/:tid', requireLogin, async (req, res) => {
     const foundUser = await User.findById(req.params.id)
     const updatetask = await todos.findOneAndUpdate({ user: foundUser.username, _id: req.params.tid }, { ...req.body.todo })
-    await updatetask.save() 
+    await updatetask.save()
     await foundUser.populate('tasks')
     await foundUser.save();
-    const data = foundUser.tasks 
+    const data = foundUser.tasks
     res.render('home', { foundUser, data })
 })
 
 //deleting the task
-app.get('/delete/:id/:tid', requireLogin,async (req, res) => {
+app.get('/delete/:id/:tid', requireLogin, async (req, res) => {
     const foundUser = await User.findById(req.params.id)
-    const task = await todos.findOneAndDelete({ user: foundUser.username, _id: req.params.tid }) 
+    const task = await todos.findOneAndDelete({ user: foundUser.username, _id: req.params.tid })
 
     await foundUser.populate('tasks')
     await foundUser.save()
-    const data = await foundUser.tasks 
+    const data = await foundUser.tasks
     res.render('home', { foundUser, data })
 })
-app.listen(3000, (req, res) => {
+
+//loggin out some one
+app.get('/logout', requireLogin, (req, res) => {
+    req.session.user_id = null
+    req.flash('success', "Logged out Successfully")
+    res.redirect('/login')
+})
+
+const port = process.env.port || 3000
+app.listen(port, (req, res) => {
     console.log("Serving on port 3000")
 })
---
+++ routes/autentication/index.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/user')
+const todos = require('../../models/task')
+const bcrypt = require('bcrypt')
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/auth/login')
+    }
+
+    next()
+}
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+//saving the new user into database
+router.post('/register', async (req, res) => {
+
+    const { password, username } = req.body
+    const isvalid = await User.findOne({ username: username })
+    if (isvalid != null) {
+        req.flash('error', 'username already exists')
+        res.redirect('/register')
+    }
+    const newuser = new User({ username, password })
+    //pre method in user model is called before newuser is going to be saved 
+    await newuser.save();
+    const foundUser = newuser
+    req.session.user_id = newuser._id
+
+    await foundUser.populate('tasks')
+    await foundUser.save()
+    const data = foundUser.tasks
+    //req.flash('success',"Registration Successfull!!")
+    res.render('home', { foundUser, data })
+})
+
+//displaying the login page
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+
+//validating the user to login
+router.post('/login', async (req, res) => {
+
+    const { username, password } = req.body
+    const foundUser = await User.findOne({ username })
+    if (foundUser != null) {
+        const isValid = await bcrypt.compare(password, foundUser.password)
+        if (isValid) {
+            req.session.user_id = foundUser._id
+
+            await foundUser.populate('tasks')
+            await foundUser.save()
+            const data = foundUser.tasks
+
+            res.render('home', { foundUser, data })
+        }
+        else {
+            req.flash('error', 'Invalid Username or password')
+            res.redirect('register')
+        }
+    }
+    else {
+        req.flash('error', 'Invalid Username or password')
+        res.redirect('register')
+    }
+})
+
+
+
+//loggin out some one
+router.get('/logout', requireLogin, (req, res) => {
+    req.session.user_id = null
+    req.flash('success', "Logged out Successfully")
+    res.redirect('login')
+})
+
+
+
+module.exports = router;
