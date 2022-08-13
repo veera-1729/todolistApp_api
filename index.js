73893cdefb3cef4f4afe364748cc283a62965f49
veera-1729 <veerabhadrudu1729@gmail.com>
2022-08-13 22:48:49 +0530

    "changing port"

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.get('/delete/:id/:tid', requireLogin, async (req, res) => {
     res.render('home', { foundUser, data })
 }) 
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 app.listen(port, (req, res) => {
     console.log("Serving on port 3000")
 })
